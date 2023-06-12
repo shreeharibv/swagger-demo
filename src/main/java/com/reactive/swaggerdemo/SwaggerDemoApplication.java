027b2e6137e0bc8149dfc7e19c2d1193811c3ded
shreeharibv <shreeharib@ivoyant.com>
2023-06-12 15:17:02 +0530

    Version fix

--- src/main/java/com/reactive/swaggerdemo/SwaggerDemoApplication.java
+++ src/main/java/com/reactive/swaggerdemo/SwaggerDemoApplication.java
@@ -2,12 +2,9 @@ package com.reactive.swaggerdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableWebMvc
+
 @SpringBootApplication
-@EnableSwagger2
 public class SwaggerDemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/reactive/swaggerdemo/config/SpringFoxConfig.java
+++ src/main/java/com/reactive/swaggerdemo/config/SpringFoxConfig.java
@@ -1,20 +1,30 @@
 package com.reactive.swaggerdemo.config;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SpringFoxConfig {
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI myOpenAPI() {
+        Contact contact = new Contact();
+        contact.setEmail("test@test.com");
+        contact.setName("test");
+        contact.setUrl("test.com");
+
+        Info info = new Info()
+                .title("Sample Open API Test")
+                .version("1.0")
+                .contact(contact)
+                .description("This API exposes endpoints to manage cases.")
+                .termsOfService("https://www.test.com/terms");
+
+        return new OpenAPI().info(info);
+
     }
 }
